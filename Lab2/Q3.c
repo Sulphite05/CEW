1c778f24a2485678bd43c7cc7cfdb87d6e37ee72
Sulphite05 <139615591+Sulphite05@users.noreply.github.com>
2023-10-29 18:31:45 +0500

    Created Q3.c

---
+++ Lab2/Q3.c
@@ -0,0 +1,22 @@
+// Write a C program to compare two strings without using string library functions.
+
+#include <stdio.h>
+int main(){
+  char s1[] = "default", s2[] = "default";
+  printf("Enter the first string: ");
+  scanf("%s", &s1);
+  printf("Enter the second string: ");
+  scanf("%s", &s2);
+  int i;
+  for(i = 0; s1[i]==s2[i]; i++);
+  if(s1[i] > s2[i]){
+    printf("%s is more than %s", s1, s2);
+  }
+  else if(s1[i] < s2[i]){
+    printf("%s is less than %s", s1, s2);
+  }
+  else{
+    printf("%s is equal to %s", s1, s2);
+  }
+  return 0;
+}
