92ff93e5215c9296773110d0dd7404e9c2cd4542
Sulphite05 <139615591+Sulphite05@users.noreply.github.com>
2023-10-30 08:30:57 +0500

    Updated Q6.c

--- Lab2/Q6.c
+++ Lab2/Q6.c
@@ -1,18 +1,21 @@
 // Write a C program to add two distances in inch-feet system using structures.
 
+struct Distance
+{
+    int ft;
+    int inch;
+};
+
 #include <stdio.h>
-struct Distance{
-      int ft;
-      int inch;
-  };
-  
 int main(){
-  
-  struct Distance dist1 = {4, 7};
-  struct Distance dist2 = {3, 2};
-  
-  struct Distance dist3 = {dist1.ft + dist2.ft, dist1.inch + dist2.inch};
-  
-  printf("%d'%d", dist3.ft, dist3.inch);
-  return 0;
+    struct Distance d1, d2;
+    printf("Enter the ft and inch of the first distance: ");
+    scanf("%d %d", &d1.ft, &d1.inch);
+
+    printf("Enter the ft and inch of the second distance: ");
+    scanf("%d %d", &d2.ft, &d2.inch);
+
+    printf("The total distance is: %d'%d\n", d1.ft+d2.ft, d1.inch+d2.inch);
+
+    return 0;
 }
